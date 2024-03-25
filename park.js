f5c9759a25c25dc47713307e1dd9580509ba50ac
kimyoungseok11 <emaonx2@gmail.com>
2024-03-25 20:24:52 +0900

    24-03-25 공원산책 알고리즘 수정

--- park.js
+++ park.js
@@ -1,15 +1,17 @@
 function solution(park, routes) {
   let startX;
   let startY;
-  let parkXLength = park.length;
-  let parkYLength = park[0].length;
+  let parkXLength = park.length-1;
+  let parkYLength = park[0].length-1;
   let answer = [];
+
   const dirRoute = {
     E: [0, 1],
     W: [0, -1],
     S: [1, 0],
     N: [-1, 0],
   };
+
   let parkArray = park.map((elem) => {
     const route = elem.split("");
     return route;
@@ -39,12 +41,12 @@ function solution(park, routes) {
     for (let i = 1; i <= moveCount; i++) {
       finX = mx + finX;
       finY = my + finY;
-      if (finX >= parkYLength || finY >= parkXLength) {
+      if (finX > parkYLength || finY > parkXLength || finX < 0 || finY < 0 || parkArray[finX][finY] === "X"){
         break;
       }
-      if (parkArray[finX][finY] === "O") {
-        count += 1;
-      }
+      
+    count += 1;
+      
     }
 
     if (moveCount === count) {
@@ -54,10 +56,5 @@ function solution(park, routes) {
   });
   answer.push(startX);
   answer.push(startY);
-  console.log(answer);
   return answer;
 }
-
-solution(["SOO", "OOO", "OOO"], ["E 2", "S 2", "W 1"]);
-solution(["SOO", "OXX", "OOO"], ["E 2", "S 2", "W 1"]);
-solution(["OSO", "OOO", "OXO", "OOO"], ["E 2", "S 3", "W 1"]);
