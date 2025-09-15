2400d17e096cf6b6e1ea91e98c493926f6eba0fd
kimyoungseok11 <emaonx2@gmail.com>
2025-09-15 21:34:56 +0900

    25-09-15 압축문제 복습

--- Squeez.java
+++ Squeez.java
@@ -8,36 +8,41 @@ public class Squeez {
         int[] answer = {};
         List<Integer> answerList = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        int nextIndex = 27;
+        int nextNUm = 27;
 
-        for(int i = 0; i < 26; i++) {
-            map.put(String.valueOf((char) ('A' + i)), i + 1);
+        for (int i = 0; i < 26; i++) {
+            int c = 65 + i;
+            String alphabet = "" + (char) c;
+            
+            map.put(alphabet, i + 1);
         }
 
         for (int i = 0; i < msg.length();) {
             String w = "" + msg.charAt(i);
             int j = i + 1;
 
-            // 2. 사전에 있는 가장 긴 w 찾기
             while (j <= msg.length()) {
-                String candidate = msg.substring(i, j);
-                if (map.containsKey(candidate)) {
-                    w = candidate;
+                String tmp = msg.substring(i, j);
+
+                if (map.containsKey(tmp)) {
+                    w = tmp;
                     j++;
                 } else {
                     break;
                 }
             }
-            
-            // 3. w의 색인 출력
+
             answerList.add(map.get(w));
-            // 4. 사전에 w+c 추가 (c는 w 다음 글자)
-            if (j <= msg.length()) {
-                map.put(msg.substring(i, j), nextIndex++);
+
+            if(j <= msg.length()) {
+                map.put(msg.substring(i, j), nextNUm);
+                nextNUm += 1;
             }
-            i += w.length(); // w 길이만큼 인덱스 이동
+
+            i = i + w.length();
         }
-        return answer;
+        
+        return answerList.stream().mapToInt(Integer::intValue).toArray();
     }
     public static void main(String[] args) {
         String msg = "KAKAO";
