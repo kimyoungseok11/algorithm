a7b6f33f13ffaba84aa652a31d7cf85d73cf9234
kimyoungseok11 <emaonx2@gmail.com>
2026-03-10 21:57:07 +0900

    문자열 뒤집기 문제 풀이

---
+++ StringDiff.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class StringDiff {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] T = br.readLine().split(" ");
+
+        String word1 = T[0];
+        String word2 = T[1];
+
+        List<Integer> sums = new ArrayList<>();
+
+        for (int i = 0; i <= word2.length() - word1.length(); i++) {
+            int cnt = 0;
+            for (int j = 0; j < word1.length(); j++) {
+                if(word1.charAt(j) != word2.charAt(i + j)) {
+                    cnt += 1;
+                }
+            }
+            sums.add(cnt);
+        }
+
+        System.out.println(Collections.min(sums));
+    }
+}
